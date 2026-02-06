86968215902005d078dd926fdffd4a66e0af429c
fakezindev <zetabr.bruno@gmail.com>
2026-02-06 14:48:16 -0300

    Criando entity Post com User aninhado

---
+++ src/main/java/com/fakezindev/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,9 @@
+package com.fakezindev.workshopmongo.repository;
+
+import com.fakezindev.workshopmongo.domain.Post;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+}
