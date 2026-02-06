8b7733e278468fd26ab4a3a1f6e30adc7846fbb7
fakezindev <zetabr.bruno@gmail.com>
2026-02-06 13:22:33 -0300

    Inserção de usuário com POST

--- src/main/java/com/fakezindev/workshopmongo/services/UserService.java
+++ src/main/java/com/fakezindev/workshopmongo/services/UserService.java
@@ -1,6 +1,7 @@
 package com.fakezindev.workshopmongo.services;
 
 import com.fakezindev.workshopmongo.domain.User;
+import com.fakezindev.workshopmongo.dto.UserDTO;
 import com.fakezindev.workshopmongo.repository.UserRepository;
 import com.fakezindev.workshopmongo.services.exeption.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,6 +24,14 @@ public class UserService {
     public User findById(String id) {
         Optional<User> obj = repo.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+    public User insert(User obj) {
+        return repo.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDto) {
+        return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
     }
 
 }
